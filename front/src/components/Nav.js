ccfac7f3aa1d9258e65105b354e9f869d5ee2552
EH-llOrJ <jth4115@naver.com>
2022-10-27 00:47:05 +0900

    css 수정

--- front/src/components/Nav.js
+++ front/src/components/Nav.js
@@ -3,27 +3,32 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className="navi">
-      <div className="nav-home-div">
-        <Link to="/">HOME</Link>
+    <div className="nav-section">
+      <div className="nav-section-inside inside-left"></div>
+      <div className="nav-section-inside inside-center">
+        <div className="nav-home-div">
+          <Link to="/">HOME</Link>
+        </div>
+        <div className="nav-home-div">
+          <Link to="/about">ABOUT</Link>
+        </div>
+        <div className="nav-home-div">
+          <Link to="/shop">SHOP</Link>
+        </div>
+        <div className="nav-home-div">
+          <Link to="/review">REVIEW</Link>
+        </div>
+        <div className="nav-home-div">
+          <Link to="/notify">NOTIFY</Link>
+        </div>
       </div>
-      <div className="nav-home-div">
-        <Link to="/about">ABOUT</Link>
-      </div>
-      <div className="nav-home-div">
-        <Link to="/shop">SHOP</Link>
-      </div>
-      <div className="nav-home-div">
-        <Link to="/review">REVIEW</Link>
-      </div>
-      <div className="nav-home-div">
-        <Link to="/notify">NOTIFY</Link>
-      </div>
-      <div className="nav-home-div">
-        <Link to="/account">ACCOUNT</Link>
-      </div>
-      <div className="nav-home-div">
-        <Link to="/shoppingbag">SHOPPING BAG</Link>
+      <div className="nav-section-inside inside-right">
+        <div className="nav-home-div">
+          <Link to="/account">ACCOUNT</Link>
+        </div>
+        <div className="nav-home-div">
+          <Link to="/shoppingbag">SHOPPING BAG</Link>
+        </div>
       </div>
     </div>
   );
